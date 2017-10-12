6e15de18e83fd32ae7cd25e06a169beefc879358
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-10-11 17:05:19 -0700

    Added math parsing

--- naivenlp.js
+++ naivenlp.js
@@ -1,14 +1,22 @@
 "use strict"
 
-const SpellChecker = require('spellchecker');
+const SpellChecker = require("spellchecker");
 const is           = require("is");
-const sw           = require('stopword');
-var stem  = require( 'wink-porter2-stemmer' );
+const sw           = require("stopword");
+const stem         = require("wink-porter2-stemmer" );
+const w2n          = require("words-to-numbers");
+const Regex        = require("regex");
 
 // Trim any multiple occuring white spaces
 function trim(str) {
    if(!is.string(str)) throw new Error("Parameter to trim must be a string.");
    return str.replace(/\s{2,}/g," ").trim();
+}
+
+// Convert words to numbers
+function words2numbers(str) {
+   if(!is.string(str)) throw new Error("Parameter to words2numbers must be a string.");
+   return w2n.wordsToNumbers(str);
 }
 
 // Lowercase all letters
@@ -28,6 +36,11 @@ function fixSpelling(str) {
    return split.join(" ");
 }
 
+function addWordToDict(str) {
+   if(!is.string(str)) throw new Error("Parmater to fixSpelling must be a string.");
+   SpellChecker.add(str);
+}
+
 // Remove stop words
 function stops(str) {
    if(!is.string(str)) throw new Error("Parmater to stops must be a string.");
@@ -40,20 +53,61 @@ function stems(str) {
    return str.split(" ").map((e) => stem(e)).join(" ");
 }
 
+// Extract math
+function extractMath(str) {
+   if(!is.string(str)) throw new Error("First parmater to extractMath must be a string.");
+   str = words2numbers(str);
+   const toFilter = ["!", "=<", "=>", "\\[", "\]", "%", "\|", "&", "~", "\,", "{", "}", "\\?", "@", "#", "’", "'", "\\."];
+   const toKeep   = ["+", "*", "^", "-", "/", "="];
+   toFilter.map((e) => {
+      let tmpReg = new RegExp(e, "g");
+      str = str.replace(tmpReg, "");
+   });
+   str = str.replace(/\w{3,}/g,"");
+
+   let split = str.split(" ");
+
+   let ret = [];
+   for(const x of split) {
+      let tmpSplit = x.split("");
+      let nums = [];
+      for(const y of tmpSplit){
+         if(toKeep.indexOf(y) !== -1) {
+            ret.push(nums.join(""));
+            nums = [];
+            ret.push(y);
+         } else {
+            nums.push(y);
+         }
+      }
+      ret.push(nums.join(""));
+   }
+
+   // console.log(str.split("+"));
+   // console.log(str.split("+").join(" "));
+   str = trim(ret.join(" "));
+   str = stops(str);
+   return str;
+}
+
 function parse(str, skip) {
    if(!is.string(str)) throw new Error("First parmater to parse must be a string.");
    if(is.object(skip)) {
-      if(!(!!skip.trim))   str = trim(str);
-      if(!(!!skip.lower))  str = lower(str);
-      if(!(!!skip.fixSp))  str = fixSpelling(str);
-      if(!(!!skip.stops))  str = stops(str);
-      if(!(!!skip.stems))  str = stems(str);
+      if(!(!!skip.trim))    str = trim(str);
+      if(!(!!skip.lower))   str = lower(str);
+      if(!(!!skip.fixSp))   str = fixSpelling(str);
+      if(!(!!skip.w2n))     str = words2numbers(str);
+      if(!(!!skip.extMath)) str = extractMath(str);
+      if(!(!!skip.stops))   str = stops(str);
+      if(!(!!skip.stems))   str = stems(str);
    } else {
       str = trim(str);
       str = lower(str);
-      // str = fixSpelling(str);
+      str = fixSpelling(str);
+      str = words2numbers(str);
+      str = extractMath(str);
       str = stops(str);
-      // str = stems(str);
+      str = stems(str);
    }
    return str;
 }
@@ -64,5 +118,6 @@ module.exports = {
    fixSpelling: fixSpelling,
    stops:       stops,
    stems:       stems,
+   extractMath: extractMath,
    parse:       parse
 }
--- sentence-test.js
+++ sentence-test.js
@@ -1,7 +1,11 @@
 const naivenlp = require("./naivenlp.js");
 
-sents = [
+const sents = [
    "What is 3 + 3",
+   "y = x + 3",
+   "What is the answer to y = x + 3",
+   "3 + what dog 2 = 2x frog - 3",
+   "ax + b = 2z +    y + 3",
    "What is the weather in Irvine",
    "Show me tweets for NASA",
    "Give me tweets for NASA",
@@ -17,6 +21,42 @@ sents = [
    "Who is Frank Sinatra"
 ]
 
+const nonsense = [
+"What’s your favorite piece of clothing you own / owned?",
+"What hobby would you get into if time and money weren’t an issue?",
+"What would your perfect room look like?",
+"How often do you play sports?",
+"What fictional place would you most like to go?",
+"What job would you be terrible at?",
+"When was the last time you climbed a tree?",
+"If you could turn any activity into an Olympic sport, what would you have a good chance at winning medal for?",
+"What is the most annoying habit that other people have?",
+"What job do you think you’d be really good at?",
+"What skill would you like to master?",
+"What would be the most amazing adventure to go on?",
+"If you had unlimited funds to build a house that you would live in for the rest of your life, what would the finished house be like?",
+"What’s the dumbest thing you’ve done that actually turned out pretty well?",
+"They say that everyone has a book in them. What would your book be about?",
+"What is something you will NEVER do again?",
+"What do you spend the most time thinking about?",
+"What are some of the events in your life that made you who you are?",
+"What do you wish your brain was better at doing?",
+"There are two types of people in this world. What are the two types?",
+"What is the strangest thing you have come across?",
+"What is something you are certain you’ll never experience?",
+"What dumb accomplishment are you most proud of?",
+"If you could make one rule that everyone had to follow, what rule would you make?",
+"What are you addicted to?"
+]
+
 for(const x of sents) {
    console.log(naivenlp.parse(x));
 }
+
+for(const x of nonsense) {
+   console.log(naivenlp.parse(x));
+}
+
+// for(const x of sents) {
+//    console.log(naivenlp.extractMath(x));
+// }
